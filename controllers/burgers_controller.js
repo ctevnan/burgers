0ff4ce45c6bc5b9f74af5fd8481c47a4f839f28b
ctevnan <carolyntevnan@yahoo.com>
2016-02-17 14:13:11 -0500

    made dir controllers, made burger_controllers.js file, imported express, router, burger, created the routes for the app

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+//get route -> index
+router.get('/', function(req,res) {
+      console.log('yesss')
+      burger.all(function(burger_data) {
+          console.log('got here!')
+          res.render('index', {burger_data: burger_data});
+      });
+});
+
+//post route -> back to index
+router.post('/create', function(req, res) {
+      burger.create(req.body.burger_name, function(result){
+           res.redirect('/');
+      });
+});
+
+//put route -> back to index
+router.put('/update', function(req, res) {
+      burger.update(req.body.burger_id, function(result) {
+             res.redirect('/');
+      });
+});
+
+//delete route -> back to index
+router.delete('/destroy', function(req, res) {
+      burger.destroy(req.body.burger_id, function(result) {
+            res.redirect('/');
+      });
+});
+module.exports = router;
