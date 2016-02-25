39e69982f0c7fadc8a5c39754730a5fa50f94c2b
ctevnan <carolyntevnan@yahoo.com>
2016-02-25 00:34:20 -0500

    added seed.sql to controllers, updated schema file with primary key

---
+++ controllers/seed.sql
@@ -0,0 +1,1 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Royale with Cheese" false), ("Blue Cheese with Bacon", false);
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,14 @@
 ### Schema
 
 CREATE DATABASE burgers_db;
+
 USE burgers_db;
 
-CREATE TABLE burger_table;
-  (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    burger_name VARCHAR(255) NOT NULL,
-    devoured tinyint(1),
-    date TIMESTAMP
-  );
+CREATE TABLE burger_table (
+  id INT AUTO_INCREMENT,
+  burger_name VARCHAR(50) NOT NULL,
+  devoured boolean NOT NULL,
+  date TIMESTAMP,
+  PRIMARY KEY (id)
+);  
+  
