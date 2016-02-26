c472404594cbd83da0c52398128258ba82da873f
ctevnan <carolyntevnan@yahoo.com>
2016-02-26 13:34:00 -0500

    adding sriracha_burger.png, working on server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var envs = require('dotenv').config();;
 var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
+//var methodOverride = require('method-override');
+//var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var PORT = process.env.NODE_ENV || 3000;
 //heroku config will make the jawsdb url for you
@@ -10,10 +10,10 @@ var app = express();
 
 //serve static content (burger.png)for the app from the "public" directory 
 //in the app directory
-app.use(express.static('public'));
+app.use('/static', express.static('/public'));
 
-app.use('method-override'('_method'));
-app.use('body-parser.urlencoded'({extended: false}));
+//app.use('method-override'('_method'));
+//app.use('body-parser.urlencoded'({extended: false}));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -25,6 +25,6 @@ app.use('/update', routes);
 
 var orm = require('./config/orm.js')
 
-app.listen(PORT, function(err, res) {
-  console.log("Listening on PORT " + PORT);
-})
+app.listen(localhost), function() {
+  console.log("Listening on PORT %s", PORT);
+};
