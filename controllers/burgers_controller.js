2289f4de44b26aa8a925a476c6bccc340f59e3bb
ctevnan <carolyntevnan@yahoo.com>
2016-02-26 20:08:57 -0500

    forgot to make delete routes in burgers_controller. added now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,24 +5,38 @@ var actions = require('../models/burger.js');
 //get route -> index
 router.get('/', function(req,res) {
   actions.seeBurg(function(burger_data) {    
-  res.render('', {burger_data});
+    res.render('', {burger_data});
   });
 });
 
 //post route -> back to index
 router.post('/create', function(req, res) {
-  actions.addBurger(req.body.entry, function(result) {
-    res.redirect('/');
+  actions.plusBurg(req.body.burger_name, function(result) {
+    console.log(result);
   });
+  res.redirect('/');
 });
 
 //put route -> back to index
 router.put('/update', function(req, res) {
-  actions.eatBurger(req.body.burger, function(result) {
-    console.log(req.body);
-    console.log(req.body.burger);
+  actions.tfBurg(req.body.burger_id, function(result) {
+    //console.log(req.body);
+    //console.log(req.body.burger);
     console.log(result);
-    res.redirect('/');
+  });
+  res.redirect('/');
+});
+//delete route -> back to index
+router.delete('/delete', function(req, res) {
+  actions.nomBurg(req.body.burger_id, function(result) {
+    console.log(result);
+  });
+  res.redirect('/')
+});
+//delete route -> back to index, burger gone
+router.delete('/remove', function(req,res) {
+  actions.noBurg(req.body.burger_id, function(result) {
+    console.log(result);
   });
 });
 
